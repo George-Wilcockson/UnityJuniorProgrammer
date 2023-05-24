bd4caedacdbd7b46ffdd4ccdc22ad27e6be2161b
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-05-24 15:20:35 +0100

    Commit007 - Upload of unit 2 "fetch" project

---
+++ Unit 2 - Fetch Challenge/Assets/Scripts/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Chnage the spawn interval to a random number
+        //spawnInterval = Random.Range(1, 3);
+
+        // Random number generator to access array
+        int arrIndex = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[arrIndex], spawnPos, ballPrefabs[arrIndex].transform.rotation);
+
+        Invoke("SpawnRandomBall", Random.Range(1.0f, 2.0f));
+        Debug.Log("Spawn Now");
+    }
+
+}
