cd3f889a86fcebcf64a3161e13077176ebdb6a20
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-05-31 14:38:19 +0100

    Commit025 - Paddle code 1st attempt
    Left flipper moves, but doesn't move back. No functionality on right paddle

--- Unit 5 - Counting Prototype/Assets/Scripts/PlayerControllerX.cs
+++ Unit 5 - Counting Prototype/Assets/Scripts/PlayerControllerX.cs
@@ -4,28 +4,55 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    private Rigidbody leftPaddle;
-    private Rigidbody rightPaddle;
+    // We don't need to access the paddles, just the springs that control them!
+
+    // Visible private field of speed to move the paddles [SerializeField]
+    // Angles of paddles
+    public float restPos = 0f;
+    public float pressedPos = 45f;
+    public float paddleStrength = 1000f;
+    public float paddleDamper = 150f;
+    HingeJoint hinge;
+
+    // The name of the axis
+    public string inputName;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        leftPaddle = GetComponent<Rigidbody>();
-        rightPaddle = GetComponent<Rigidbody>();
+        // Initialise the hinges
+        hinge = GetComponent<HingeJoint>();
+        // Hinge uses the spring skill
+        hinge.useSpring = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("a"))
+        // Set the springs values
+        JointSpring spring = new JointSpring();
+        spring.spring = paddleStrength;
+        spring.damper = paddleDamper;
+        // Move left paddle
+        if(Input.GetAxis(inputName) == 1)
         {
-            Debug.Log("'a' was pressed");
+            spring.targetPosition = pressedPos;
+
+        }
+        else
+        {
+            spring.targetPosition = restPos;
         }
 
-        if(Input.GetKey("d"))
-        {
-            Debug.Log("'w' was pressed");
-        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
 
+                // Move right paddle
+        if(Input.GetKey("s"))
+        {
+            Debug.Log("'s' was pressed");
+        }
+        
     }
 }
