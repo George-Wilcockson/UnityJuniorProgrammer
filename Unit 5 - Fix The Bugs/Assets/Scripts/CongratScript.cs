c17ec975ca63475069335a248550795bb965468d
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-01 09:26:15 +0100

    Commit040 - Compile errors dealt with

---
+++ Unit 5 - Fix The Bugs/Assets/Scripts/CongratScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+    
+    private static List<string> textToDisplay = new List<string>();
+    
+    private float rotatingSpeed;
+    private float timeToNextText;
+
+    private int currentText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToNextText = 0.0f;
+        currentText = 0;
+        
+        rotatingSpeed = 1.0f;
+
+        textToDisplay.Add("Congratulation");
+        textToDisplay.Add("All Errors Fixed");
+
+        Text.text = textToDisplay[0];
+        
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToNextText += Time.deltaTime;
+
+        if (timeToNextText > 1.5f)
+        {
+            timeToNextText = 0.0f;
+            
+            currentText++;
+            if (currentText >= textToDisplay.Count)
+            {
+                currentText = 0;
+            }
+
+            Text.text = textToDisplay[currentText];
+        }
+    }
+}
